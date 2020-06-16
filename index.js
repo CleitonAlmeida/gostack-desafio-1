37181ea745132ecbc3d5fa1abf085822e97591b3
Cleiton Almeida <cleitonant@gmail.com>
2020-06-15 23:10:57 -0300

    Add POST /projects route

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const server = express();
+//To use json on requisitions
+server.use(express.json());
 
 const projects = [
     {
@@ -14,4 +16,19 @@ server.get('/projects', (req, res) => {
     return res.json(projects);
 });
 
-server.listen(3000)
+server.post('/projects', (req, res) => {
+    const id = req.body.id;
+    const title = req.body.title;
+
+    projects.push( 
+        {
+            id: id,
+            title: title,
+            tasks: []
+        }
+    );
+
+    return res.json({'message': 'Project created!'});
+});
+
+server.listen(3000);
